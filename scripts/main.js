66457d1121343c0d56a99f850932f10a9c2063b1
prg-77 <pabloramgav@gmail.com>
2024-09-12 12:03:01 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// const miTitulo = document.querySelector("h1");
+// miTitulo.textContent = "¡Hola mundo!";
+document.querySelector("img").onclick = function () {
+    alert("¡Ouch! ¡Deja de pincharme!");
+  };
